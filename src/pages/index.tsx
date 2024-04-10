c8005c2bb11dcbe87e97008acdfd62150a492daa
Bruno Feitoza <brunofeitozanascimento@gmail.com>
2024-04-10 01:20:22 -0300

    add `.editorconfig`, `.nvmrc`, rules in `.eslintrc.json` files and install `prettier` with `plugin-tailwindcss`

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import { poppins } from "@/styles/fonts";
+import { poppins } from '@/styles/fonts'
 
 export default function Home() {
   return (
     <main className={`${poppins.className}`}>
       <h1>Home</h1>
     </main>
-  );
+  )
 }
