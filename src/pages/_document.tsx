0f47ac63a88cdade82bf1797859ba399d23cec1a
Bruno Feitoza <brunofeitozanascimento@gmail.com>
2024-04-11 20:51:37 -0300

    refactor(linter): update eslint configs
    remove @rocketseat/eslint-config

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,11 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { poppins } from '@/styles/fonts'
 
 export default function Document() {
   return (
     <Html lang="pt-br">
       <Head />
-      <body>
+      <body className={`${poppins.className} antialiased`}>
         <Main />
         <NextScript />
       </body>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import { poppins } from '@/styles/fonts'
 
 export default function Home() {
   return (
-    <main className={`${poppins.className}`}>
+    <main className={`${poppins.className} antialiased`}>
       <h1>Home</h1>
     </main>
   )
