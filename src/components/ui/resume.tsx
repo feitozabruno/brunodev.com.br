0cf9dc227cea73da6d2002cdf74c69a93a518809
Bruno Feitoza <brunofeitozanascimento@gmail.com>
2024-06-12 17:44:24 -0300

    refactor: add resume component with apresentation

---
+++ src/components/ui/resume.tsx
@@ -0,0 +1,16 @@
+import { InfoBadges } from './info-badge'
+
+export function Resume() {
+  return (
+    <div className='mt-8 text-sm'>
+      <p className='text-left leading-loose md:text-base'>
+        👨🏾‍💻Especialista em projetar, desenvolver e manter aplicações web. Escrevo
+        código limpo, padronizado, testado e bem documentado. Utilizo boas
+        práticas de desempenho e segurança, além de garantir a otimização (SEO)
+        para motores de busca e a compatibilidade com diferentes navegadores.
+      </p>
+
+      <InfoBadges />
+    </div>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
 import { Intro } from '@/components/ui/intro'
-import { InfoBadges } from '@/components/ui/info-badge'
 import { ProjectList } from '@/components/ui/projects'
 import { Footer } from '@/components/ui/footer'
 import { Divisor } from '@/components/ui/divisor'
 import { NavMenu } from '@/components/ui/nav-menu'
 import { Container } from '@/components/ui/container'
+import { Resume } from '@/components/ui/resume'
 
 export default function Home() {
   return (
@@ -13,18 +13,7 @@ export default function Home() {
 
       <Container>
         <Intro />
-
-        <div className='mt-8 text-sm'>
-          <p className='text-left leading-loose md:text-base'>
-            👨🏾‍💻Especialista em projetar, desenvolver e manter aplicações web.
-            Escrevo código limpo, padronizado, testado e bem documentado.
-            Utilizo boas práticas de desempenho e segurança, além de garantir a
-            otimização (SEO) para motores de busca e a compatibilidade com
-            diferentes navegadores.
-          </p>
-
-          <InfoBadges />
-        </div>
+        <Resume />
 
         <Divisor />
 
